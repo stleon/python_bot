bd7efa2978b2c5f122ac2d4e0590e21e57caaa76
r.averichev <r.averichev@rambler-co.ru>
2016-04-23 20:57:47 +0300

    re.sub added

--- provider.py
+++ provider.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import time
 import pexpect
+from pexpect.exceptions import TIMEOUT
 
 # Don't do this unless you like being John Malkovich
 # c = pexpect.spawnu('/usr/bin/env python ./python.py')
@@ -19,13 +21,13 @@ class PythonProvider:
     def get_connection_by(self, user_id):
         user_conn = self._connections.get(user_id)
         if not user_conn:
-            user_conn = pexpect.spawnu('/usr/bin/env python')
-            self._connections[user_id] = new_user_connection
+            user_conn = pexpect.spawnu('/usr/bin/env python3')
+            self._connections[user_id] = user_conn
         return user_conn
 
     def custom_commands(self, user_id, command):
         if command == 'exit()':
-            conn = self.get_user_connection_by(user_id)
+            conn = self.get_connection_by(user_id)
             conn.kill(0)
             del self._connections[user_id]
             user_id, command = None, None
@@ -45,12 +47,23 @@ class PythonProvider:
 
         conn = self.get_connection_by(user_id)
 
+        command = '\n'.join([command, 'print("EOFLINE")'])
         conn.sendline(command)
 
-        result = ''.join(conn.readline())
-        return result
+        result = ''
+        while True:
+            new_line = conn.readline()
+            if 'EOFLINE' in new_line:
+                new_line = conn.readline()
+                break
+            result += new_line
 
+        re.sub(r'>>>.+\n', '', result)
+
+        return result
 
 if __name__ == '__main__':
     provider = PythonProvider()
+    result = provider.execute_command(1, 'import this')
+    print(result)
 
