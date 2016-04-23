b1cd8ec7a5f98921daddc817a493ca679b30f205
STLEON <leonst998@gmail.com>
2016-04-23 18:12:23 +0300

    subprocess

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import sys
 import asyncio
+import subprocess
 import telepot
 import telepot.async
+
+from provider import PythonProvider
 
 
 class PythonBot(telepot.async.Bot):
@@ -13,7 +16,7 @@ class PythonBot(telepot.async.Bot):
     @asyncio.coroutine
     def on_chat_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
-        print('Chat Message:', content_type, chat_type, chat_id, msg) # 117945175
+        print('Chat Message:', content_type, chat_type, chat_id, msg)
         if msg['text'] == '/about':
             yield from self.sendMessage(chat_id, 'About page!')
         elif msg['text'] == '/ping':
@@ -21,33 +24,53 @@ class PythonBot(telepot.async.Bot):
         elif msg['text'] == '/help':
             yield from self.sendMessage(chat_id, 'Help page')
         elif msg['text'] == '/python':
-            yield from self.sendMessage(chat_id, 'Party starts')
+            pass
+            #provider = PythonProvider()
+            #r = provider.execute_command(user_id=chat_id, command='import this')
+            #yield from self.sendMessage(chat_id, r)
         elif msg['text'] == '/end':
             yield from self.sendMessage(chat_id, 'Nice')
+        else:
+            with subprocess.Popen("python3.5",
+                                  shell=True,
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  universal_newlines=True) as p:
+                out, err = p.communicate(msg['text'])
 
+            yield from self.sendMessage(chat_id, out + err)
+            #provider = PythonProvider()
+            #r = provider.execute_command(user_id=chat_id, command=msg['text'])
+            #yield from self.sendMessage(chat_id, r)
 
     def on_callback_query(self, msg):
-        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        query_id, from_id, query_data = telepot.glance(msg,
+                                                       flavor='callback_query')
         print('Callback Query:', query_id, from_id, query_data)
 
     def on_inline_query(self, msg):
-        query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+        query_id, from_id, query_string = telepot.glance(msg,
+                                                         flavor='inline_query')
         print('Inline Query:', query_id, from_id, query_string)
 
         def compute_answer():
             articles = [{'type': 'article',
-                            'id': 'abc', 'title': query_string, 'message_text': query_string}]
+                         'id': 'abc',
+                         'title': query_string,
+                         'message_text': query_string}]
 
             return articles
 
         self._answerer.answer(msg, compute_answer)
 
     def on_chosen_inline_result(self, msg):
-        result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
+        result_id, from_id, query_string = telepot.glance(
+            msg, flavor='chosen_inline_result')
         print('Chosen Inline Result:', result_id, from_id, query_string)
 
 
-TOKEN = os.getenv('TOKEN') # sys.argv[1]
+TOKEN = os.getenv('TOKEN')  # sys.argv[1]
 
 bot = PythonBot(TOKEN)
 loop = asyncio.get_event_loop()
