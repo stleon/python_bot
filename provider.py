194eb767e8ca50a014138d288ba74764ff8e02f4
ubuntu <alwil@nxt.ru>
2016-06-05 13:37:49 +0300

    execution result added

--- provider.py
+++ provider.py
@@ -8,8 +8,9 @@ class PythonProvider:
         self.mk_manager = MultiKernelManager()
 
     def get_connection_by(self, user_id):
-        kernel_manager = self.mk_manager.get_kernel(user_id)
-        if not kernel_manager:
+        try:
+            kernel_manager = self.mk_manager.get_kernel(user_id)
+        except KeyError:
             self.mk_manager.start_kernel(kernel_id=user_id)
             kernel_manager = self.mk_manager.get_kernel(user_id)
         user_client = kernel_manager.client()
@@ -33,17 +34,21 @@ class PythonProvider:
             return None
 
         cl = self.get_connection_by(user_id)
-        msg = cl.execute(command)
-        print("returned by execute")
-        pprint(msg)
+        cl.execute(command)
+
+        print('\n{}\n'.format('-'*85))
+        print(command)
 
         output = ''
         while True:
             msg = cl.get_iopub_msg()
-            print("read from iopub")
-            #print(msg)
-            if msg['msg_type'] ==  'stream':
+            pprint(msg)
+            if msg['msg_type'] == 'execute_result':
+                output += msg['content']['data']['text/plain']
+            elif msg['msg_type'] == 'stream':
                 output += msg['content']['text']
+            elif msg['msg_type'] == 'error':
+                output += '\n'.join(msg['content']['traceback'])
             elif msg.get('content', {}).get('execution_state') == 'idle':
                 break
         return output
@@ -51,18 +56,24 @@ class PythonProvider:
 
 if __name__ == '__main__':
     provider = PythonProvider()
-    result = provider.execute_command('a = 1')
+    result = provider.execute_command(1, 'asdf')
     print(result)
-    result = provider.execute_command('b = 2\nprint(a)')
+    result = provider.execute_command(1, 'a = 1')
     print(result)
-    result = provider.execute_command('''for i in range(10): \n print(i)''')
+    result = provider.execute_command(1, 'a')
     print(result)
-    result = provider.execute_command('import this')
+
+'''
+    result = provider.execute_command(1, 'b = 2\nprint(a)')
     print(result)
-    result = provider.execute_command('import this')
+    result = provider.execute_command(1, 'for i in range(10): \n print(i)')
+    print(result)
+    result = provider.execute_command(1, 'import this')
+    print(result)
+    result = provider.execute_command(1, 'import this')
     print(result)
 
     command = 'a=1\nprint(a)\nfrom time import sleep\nsleep(8)\nprint(100)'
-    result = provider.execute_command(command)
+    result = provider.execute_command(1, command)
     print(result)
-
+'''
